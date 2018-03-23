a1e13a0412a555e63ec34e674b1ef34f19a90e61
Adrian Messado <adrian.messado@gmail.com>
2018-03-23 13:05:59 -0700

    identify what language is most suited for each response of each question and place in scripts.js commented out

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,38 @@
 
+// 1 = JavaScript
+// 2 = Python
+// 3 = Ruby
+// 4 = Swift  (iOS)
+// 5 = Java (Android)
+// 6 = HTML5 (Web Design - how things look)
+// 7 = 1. JavaScript  2. HTML5  [UX: How to interact with things]
+// 8 = Python - Ruby - PHP Code = Logic
+//--------------OS ---------------------------
+// Windows = C#
+
+// Mac os x = C++ || swift || C || Objective-C
+
+// linux = C/C++  || Java || Python
+//-------------Mobile OS---------------------------------
+// iOS = Swift
+// Android = Java
+//-------------why learn---------------------------------
+// Build something fun = HTML 5 + Java + C# + Swift
+//start-up = JavaScript
+//something fun = HTML 5
+//another skill = HTML 5
+//-------------type of developer---------------------------------
+// Game = C#
+// Web = . HTML52. JavaScript
+// Mobile = Swift || Java || C#
+// Desktop = Java
+// Data Scientist = Python
+//-------------find most interesting---------------------------------
+//UX/UI = 1. JavaScript  2. HTML5
+//how things work = java
+//-------------type of companies--------------------------------
+// small business = PHP
+// startup = JavaScript
+// Enterprise = Java
+$(document).ready(function()
+{    $("#results").click(function() {
