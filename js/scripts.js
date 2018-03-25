0650e5479f17d320284300a719763871f905358d
Adrian Messado <adrian.messado@gmail.com>
2018-03-25 16:44:58 -0700

    finish creating functions for radio buttons, add submit button, auto hide submit button after pushed

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,42 @@
 $(function(){
 $("button#getStarted").click(function() {
   $(".panel-body#operatingSystem").slideDown();
-
+  $("button#getStarted").fadeOut();
+  });
 });
-});
 
-//next buttons will hide question, show next question, and show new next button for new question. repeats until no more questions. 
+//next buttons will hide question, show next question, and show new next button for new question. repeats until no more questions.
 $(function() {
-  $("input").click(function(){
+  $(".panel-body#operatingSystem, .input").click(function(){
     $("button#nextPhoneOS").fadeIn();
     $("button#nextPhoneOS").click(function(){
       $(".panel-body#operatingSystem").fadeOut();
       $(".panel-body#phoneOS").fadeIn();
+      $("button#nextPhoneOS").fadeOut();
+      $("button#nextWhyCode").fadeIn();
+      $("button#nextWhyCode").click(function() {
+        $("button#nextWhyCode").fadeOut();
+        $(".panel-body#phoneOS").fadeOut();
+        $(".panel-body#whyCode").fadeIn();
+        $("button#nextDeveloperType").fadeIn();
+        $("button#nextDeveloperType").click(function(){
+          $(".panel-body#whyCode").fadeOut();
+          $("button#nextDeveloperType").fadeOut();
+          $("button#nextMoreInteresting").fadeIn();
+          $(".panel-body#moreInteresting").fadeIn();
+          $("button#nextMoreInteresting").click(function(){
+            $(".panel-body#moreInteresting").fadeOut();
+            $("button#nextMoreInteresting").fadeOut();
+            $(".panel-body#whatCompany").fadeIn();
+            $("button#submit").fadeIn();
+            $("button#submit").click(function(){
+              $(".panel-body#whatCompany").fadeOut();
+              $("button#submit").fadeOut();
+            });
+          });
+
+        });
+      });
     });
   });
 });
